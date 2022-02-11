1c2bedb6a71de6677d30099658a787ecefe966f7
AlissaMaximo <alissahiratamaximo@gmail.com>
2022-02-11 13:55:08 -0300

    Requisito 1 completo.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Album from './components/Album';
+import Favorites from './components/Favorites';
+import Login from './components/Login';
+import NotFound from './components/NotFound';
+import Profile from './components/Profile';
+import ProfileEdit from './components/ProfileEdit';
+import Search from './components/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <Route path="/" component={ Login } />
+        <Route path="/search" component={ Search } />
+        <Route path="/album/:id" component={ Album } />
+        <Route path="/favorites" component={ Favorites } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="/profile/edit" component={ ProfileEdit } />
+        <Route path="*" component={ NotFound } />
+      </BrowserRouter>
+    );
   }
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  render() {
+    return (
+      <div data-testid="page-login">
+        texto temporario
+      </div>
+    );
+  }
+}
+
+export default Login;
